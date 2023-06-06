de1b7202bf304541417bd2d499fc9ca25788b0f5
JAshMe <mailme.jyotmehta@gmail.com>
2023-06-06 17:21:55 -0400

    chore[A2M-277] Setup React Testing Framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
 import {useState} from "react";
 import {invoke} from "@tauri-apps/api/tauri";
 import "./App.css";
+import {Greeting} from "./components/landing/Greeting";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  return (
-    <div className="container">
-      <h1>Welcome to Analog ML Connect!</h1>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
-  );
+    console.log("Rendering..");
+    return (
+        <div className="container">
+            <Greeting title={"Welcome to Analog ML Connect"}/>
+        </div>
+    );
 }
 
 export default App;
