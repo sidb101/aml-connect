a632a55c4c7a8481e9dcd450c09ac10850b8b1e6
JAshMe <mailme.jyotmehta@gmail.com>
2023-06-14 15:26:39 -0400

    chore[A2M-341]: Clened up for Demo

--- src/components/storybook/StoryContent.tsx
+++ src/components/storybook/StoryContent.tsx
@@ -7,30 +7,35 @@ import {faArrowLeft} from "@fortawesome/free-solid-svg-icons/faArrowLeft";
 export const StoryContent = () => {
     return (
         <div className={"xlight-panel content-container StoryContent_container"}>
-            <div className={"main-heading-text"}>Main Heading</div>
+            <div className={"main-heading-text"}>Projects</div>
             <div className={"white-panel  StoryContent_projectPanel"}>
-                <div className={"section-heading-text"}>Section Heading</div>
+                <div className={"section-heading-text"}>Example Project: Dog Bark Detection</div>
                 <br/>
-                <div className={"section-subheading-text"}> Section Subheading</div>
-                <div className={"section-subheading-text green-text"}> Section Blue Subheading</div>
-                <div className={"section-subheading-text grey-text"}> Section Grey Subheading</div>
+                <div className={"section-subheading-text"}> Created By: ChipMonks</div>
+                {/*<div className={"section-subheading-text green-text"}> Section Blue Subheading</div>*/}
+                {/*<div className={"section-subheading-text grey-text"}> Section Grey Subheading</div>*/}
                 <br/>
-                <div className={"regular-text"}> This is regular text.</div>
-                <div className={"regular-text grey-text"}> This is regular grey text.</div>
-                <div className={"regular-text light-grey-text"}> This is regular light-grey text.</div>
-                <div className={"small-text light-grey-text"}> This is small light-grey text.</div>
-                <div className={"xsmall-text light-grey-text"}> This is xsmall light-grey text.</div>
-            </div>
-            <div className={"StoryContent_buttonContainer"}>
-                <button className={"btn btn-solid"}>
-                    Sample Solid Button &nbsp;
+                {/*<div className={"regular-text"}> This is regular text.</div>*/}
+                {/*<div className={"regular-text grey-text"}> This is regular grey text.</div>*/}
+                <div className={"regular-text light-grey-text"}> An AnalogML model that detects the sound of dog bark for deployment in the outdoor context. It is trained on a near and far field dataset of dog barks of common American dog breeds. It employs 4 analog features and uses a 2-layer neural network. </div>
+                {/*<div className={"small-text light-grey-text"}> This is small light-grey text.</div>*/}
+                {/*<div className={"xsmall-text light-grey-text"}> This is xsmall light-grey text.</div>*/}
+                <br/>
+                <button className={"btn btn-outline"} >
+                    Open  &nbsp;&nbsp;
                     <FontAwesomeIcon icon={faArrowRight}/>
                 </button>
-                <button className={"btn btn-outline"}>
-                    <FontAwesomeIcon icon={faArrowLeft}/> &nbsp;
-                    Sample Outline Button
-                </button>
             </div>
+            {/*<div className={"StoryContent_buttonContainer"}>*/}
+            {/*    <button className={"btn btn-solid"}>*/}
+            {/*        Sample Solid Button &nbsp;*/}
+            {/*        <FontAwesomeIcon icon={faArrowRight}/>*/}
+            {/*    </button>*/}
+            {/*    <button className={"btn btn-outline"}>*/}
+            {/*        <FontAwesomeIcon icon={faArrowLeft}/> &nbsp;*/}
+            {/*        Sample Outline Button*/}
+            {/*    </button>*/}
+            {/*</div>*/}
         </div>
     )
 }
--- src/components/storybook/StoryNav.tsx
+++ src/components/storybook/StoryNav.tsx
@@ -4,10 +4,10 @@ export const StoryNav = () => {
     return (
         <div className={"dark-panel nav-container StoryNav_container"}>
             <div className={"brand-text"}>Analog ML Connect</div>
-            <br/>
-            <div className={"regular-text light-text"}>NAV TITLE</div>
-            <br/>
-            <div className={"regular-text white-text StoryNav_link"}>This is white Navbar text</div>
+            {/*<br/>*/}
+            {/*<div className={"regular-text light-text"}>NAV TITLE</div>*/}
+            {/*<br/>*/}
+            {/*<div className={"regular-text white-text StoryNav_link"}>This is white Navbar text</div>*/}
         </div>
     )
 }
