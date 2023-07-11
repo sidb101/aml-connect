89b574c26258bc8db3cc7630f020fa5968139f71
DisguisedFlyer <tburgess@gmail.com>
2023-07-11 18:30:28 -0400

    feature/landing[A2M-344]: Working and tested

--- src/layouts/landingPage/LandingPage.tsx
+++ src/layouts/landingPage/LandingPage.tsx
@@ -1,10 +1,7 @@
 import { Header } from "./components/Header";
-import "./LandingPage.scss";
-import { ExampleProject } from "./components/ExampleProject";
-import { NavRegion } from "../../components/navRegion/NavRegion";
-import openProjectsSections from "../../components/navRegion/mockData/openedProject";
-import { Sidebar } from "../../components/sideBar/Sidebar";
 import React from "react";
+import { ExampleProject } from "./components/ExampleProject";
+import "./LandingPage.scss";
 
 export const LandingPage = () => {
 	return (
@@ -12,38 +9,26 @@ export const LandingPage = () => {
 			<div className={`header-content-container`}>
 				<Header />
 			</div>
-			<div className={`main-content-container`}>
-				<p>
-					Lonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnng
-				</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				<p>Test</p>
-				{/*<ExampleProject projectName="Example Project: Dog Bark Detection" />*/}
-				{/*<ExampleProject projectName="Glass Break Detection" />*/}
-				{/*<ExampleProject projectName="Alexa Voice" />*/}
+			<div className={`body-content-container`}>
+				<div className={`main-content-container`}>
+					{/* <div> */}
+					{/*	Testsadklajasdffffffffffffffffffffffffffffffffffffffffasdfsadfsajasdjhkgjkhfdjhkgfjkhldgfgfdhbdgfsbgdfjkhlgsdjhklsdajhdgajhklgjhkdgfjdgfgfdlfsssssssssssssssssssssssssssssssssssssss|tttt */}
+					{/* </div> */}
+					<div className={`LandingPage_gridContainer`}>
+						<div className={`LandingPage_gridRow1`}>
+							<ExampleProject projectName="Example Project: Dog Bark Detection" />
+							<ExampleProject projectName="Glass Break Detection" />
+						</div>
+						<div className={`LandingPage_gridRow2`}>
+							<ExampleProject projectName="Alexa Voice" />
+							<ExampleProject projectName="Alexa Voice" />
+						</div>
+						<div className={`LandingPage_gridRow3`}>
+							<ExampleProject projectName="Alexa Voice" />
+							<ExampleProject projectName="Alexa Voice" />
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
--- src/layouts/landingPage/components/Header.tsx
+++ src/layouts/landingPage/components/Header.tsx
@@ -11,34 +11,32 @@ export const Header = () => {
 
 	return (
 		<>
-			<div>
-				<div className={`Header_gridContainer`}>
-					<div className={`Header_gridRow1`}>
-						<div className={`main-heading-text`}>Projects</div>
+			<div className={`Header_gridContainer`}>
+				<div className={`Header_gridRow1`}>
+					<div className={`main-heading-text`}>Projects</div>
+				</div>
+				<div className={`Header_gridRow2`}>
+					{isSelected ? (
+						<div
+							className={`Header_backDrop`}
+							onClick={() => {
+								expandPopup();
+							}}
+						></div>
+					) : null}
+					<div className={`Header_popupContainer`}>
+						<button
+							className={`btn ${isSelected ? `btn-solid` : `btn-outline`}`}
+							onClick={() => {
+								expandPopup();
+							}}
+						>
+							Create New Project +
+						</button>
 					</div>
-					<div className={`Header_gridRow2`}>
-						{isSelected ? (
-							<div
-								className={`Header_backDrop`}
-								onClick={() => {
-									expandPopup();
-								}}
-							></div>
-						) : null}
-						<div className={`Header_popupContainer`}>
-							<button
-								className={`btn ${isSelected ? `btn-solid` : `btn-outline`}`}
-								onClick={() => {
-									expandPopup();
-								}}
-							>
-								Create New Project +
-							</button>
-						</div>
-					</div>
-					<div className={`Header_gridRow3`}>
-						<div className={`Header_popupContainer`}>{isSelected ? <CreateNewProjectPopup /> : null}</div>
-					</div>
+				</div>
+				<div className={`Header_gridRow3`}>
+					<div className={`Header_popupContainer`}>{isSelected ? <CreateNewProjectPopup /> : null}</div>
 				</div>
 			</div>
 		</>
