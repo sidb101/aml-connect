b5540e6dff7751fc36827495afe376568701134d
sbhatna2 <sbhatna2@river.wifi.local.cmu.edu>
2023-07-13 17:05:55 -0400

    feature[A2M-354] Saving progress, TODO: rust tests

--- src-tauri/src/aml_core/element_repository.rs
+++ src-tauri/src/aml_core/element_repository.rs
@@ -1,5 +1,34 @@
 use tauri::api::process::Command;
 pub fn list_elements_from_simulator() -> String {
-    let output = Command::new_sidecar("aspinity_wrapper").expect("Command Failed").args(["--get_elements"]).output().unwrap();
+    let command_result = Command::new_sidecar("aspinity_wrapper")
+
+    match command_result {
+        Ok(value) => {
+            let command_handle = value;
+        }
+        Err(error) => {
+            println!("Error while loading command: {}", error);
+        }
+    };
+    
+
+    let mut command;
+    match command_result {
+        Command => 
+    }
+
+    // If you need to specify a failure, use an else:
+    if let Command(command_result) = letter {
+        println!("Matched {:?}!", i);
+    } else {
+        // Destructure failed. Change to the failure case.
+        println!("Didn't match a number. Let's go with a letter!");
+    }
+
+    let output = Command::new_sidecar("aspinity_wrapper")
+        .expect("Command Failed")
+        .args(["--get_elements"])
+        .output()
+        .unwrap();
     output.stdout
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,28 @@
 pub mod aml_core;
+use tauri::api::process::Command;
+use core::fmt::Error;
 
-pub mod uicontroller {    
+pub fn execute_sidecar_and_fetch_output(sidecar: &str, param: &str) -> Result<String, Error> {
+    let sidecar_command_result = Command::new_sidecar(sidecar);
+    let mut command_handle: Command;
+    match sidecar_command_result {
+        Ok(value) => {
+            command_handle = value;
+        }
+        Err(error) => {
+            println!("Error while loading command: {}", error);
+            return;
+        }
+    };
+    
+    let command_handle_with_args = command_handle.args(["--get_elements"]);
+    let exec_result = command_handle_with_args.output();
+
+    match sidecar_
+
+}
+
+pub mod uicontroller {
     #[tauri::command]
     pub fn get_elements() -> String {
         crate::aml_core::element_repository::list_elements_from_simulator()
