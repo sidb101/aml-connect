9d2e30b44929d931eb593a96c563bb814b98934a
sidb101 <bhatnagar011096@gmail.com>
2023-07-20 10:47:12 -0400

    Refactored code for DI, added test

--- src-tauri/src/aml_core/element_repository.rs
+++ src-tauri/src/aml_core/element_repository.rs
@@ -1,8 +1,10 @@
-use crate::utils::{self, Execute};
+use crate::utils::Execute;
 
-pub fn list_elements_from_simulator() -> String {
-    let sidecar = utils::Sidecar::new(String::from("aspinity_wrapper"));
-    let result = sidecar.execute_sidecar_and_fetch_output(&["--get_elements"]);
+pub fn list_elements_from_simulator<E: Execute>(executable: &E) -> String {
+    let result = executable.fetch_output(
+        String::from("aspinity_wrapper"),
+        vec!["--get_elements".to_string()],
+    );
     match result {
         Ok(elements) => elements,
         Err(err) => format!("Failed to fetch Network Elements from Simulator: {}", err),
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,77 +1,79 @@
 pub mod aml_core;
 
 pub mod utils {
-    use tauri::api::process::{Command, Output};
+    use mockall::predicate::*;
+    use mockall::*;
+    use tauri::api::process::Command;
 
+    #[automock]
     pub trait Execute {
-        fn execute_sidecar_and_fetch_output(
-            &self,
-            params: &[&str],
-        ) -> Result<String, Box<dyn std::error::Error>>;
+        fn fetch_output(&self, filename: String, params: Vec<String>) -> Result<String, String>;
     }
 
-    pub struct Sidecar {
-        filename: String
-    }
-
-    impl Sidecar {
-        pub fn new(filename: String) -> Sidecar {
-            Sidecar {filename}
-        }
-    }
+    pub struct Sidecar {}
 
     impl Execute for Sidecar {
-        fn execute_sidecar_and_fetch_output(
-            &self,
-            params: &[&str],
-        ) -> Result<String, Box<dyn std::error::Error>> {
-            let sidecar = &self.filename;
-            let command: Command = Command::new_sidecar(sidecar)?.args(params);
-            let command_output: Output = command.output()?;
-            let stdout: String = command_output.stdout;
-        
-            match command_output.status.success() {
-                true => Ok(stdout),
-                false => Err(format!("Command failed with exit code: {:?}", command_output.status).into()),
-            }
+        fn fetch_output(&self, filename: String, params: Vec<String>) -> Result<String, String> {
+            let command_result = Command::new_sidecar(&filename);
+            let command_with_args = match command_result {
+                Ok(command) => command.args(&params),
+                Err(_e) => {
+                    return Err(format!(
+                        "Could not build command for filename: {}",
+                        &filename
+                    ));
+                }
+            };
+
+            let command_output_result = command_with_args.output();
+            let command_output = match command_output_result {
+                Ok(command_output) => command_output,
+                Err(_e) => {
+                    return Err("Failed to execute the command".to_string());
+                }
+            };
+
+            Ok(command_output.stdout)
         }
     }
-
-    
-
-        
 }
 
 pub mod uicontroller {
+    use crate::utils::Sidecar;
+
     #[tauri::command]
     pub fn get_elements() -> String {
-        crate::aml_core::element_repository::list_elements_from_simulator()
+        let sidecar: Sidecar = Sidecar {};
+        crate::aml_core::element_repository::list_elements_from_simulator(&sidecar)
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use double::*;
+#[cfg(test)]
+mod tests {
+    use crate::aml_core::element_repository;
+    use crate::utils::*;
+    use mockall::*;
 
-//     mock_trait!(
-//         MockCommand,
-//         new_sidecar(u64) -> f64);
-    
-//     impl new_sidecar for MockCommand {
-//         mock_method!((&self, timestamp: u64) -> f64);
-//     }
+    #[test]
+    fn test_list_elements_calls_fetch_output_with_correct_params() {
+        // arrange
+        let expected_param1: String = String::from("aspinity_wrapper");
+        let expected_param2: Vec<String> = vec!["--get_elements".to_string()];
+        let expected_output: Result<String, String> = Ok("Dummy Elements".to_string());
 
+        let mut mock = MockExecute::new();
+        mock.expect_fetch_output()
+            .with(
+                predicate::eq(expected_param1),
+                predicate::eq(expected_param2),
+            )
+            .times(1)
+            .return_const(expected_output.clone());
 
-//     use 
-//     mock_trait!(MockCommand, new_sidecar(String) -> Result<Self, Err>);
-    
-//     impl ProfitModel for MockCommand {
-//         mock_method!(profit_at(&self, timestamp: u64) -> f64);
-//     }
+        // act
+        let output = element_repository::list_elements_from_simulator(&mock);
 
-//     #[test]
-//     fn it_works() {
-//         let res = utils::execute_sidecar_and_fetch_output(sidecar, params);
-//     }
-// }
+        //assert
+        assert_eq!(expected_output.unwrap(), output);
+    }
+}
