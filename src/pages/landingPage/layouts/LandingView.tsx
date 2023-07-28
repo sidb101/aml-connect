fdc2460e5a80e6079126fd6596dc0ecd4d706373
DisguisedFlyer <tburgess@gmail.com>
2023-07-28 16:30:49 -0400

    feature[A2M-537]: Removed unused classes in LandingView scss

--- src/pages/landingPage/layouts/LandingView.tsx
+++ src/pages/landingPage/layouts/LandingView.tsx
@@ -1,5 +1,4 @@
 import { LandingPageHeader } from "./components/LandingPageHeader";
-import { ExampleProject } from "./components/ExampleProject";
 import React from "react";
 import "./LandingView.scss";
 import Header from "../../../components/header/Header";
@@ -28,40 +27,7 @@ const LandingView = (props: LandingPageViewT) => {
 							<ProjectCard />
 							<ProjectCard />
 						</div>
-						<div className={`LandingView_containerRow`}>
-							<ExampleProject projectName="Glass Break Detection" />
-							<ExampleProject projectName="Glass Break Detection" />
-						</div>
-						{/*<ExampleProject projectName="Glass Break Detection" />*/}
 					</div>
-					{/*	<div className={`LandingPageView_gridContainer`}>*/}
-					{/*		<div className={`LandingPageView_gridRow1`}>*/}
-					{/*			<ProjectCard />*/}
-					{/*			<ExampleProject projectName="Example Project: Dog Bark Detection" />*/}
-					{/*			<ExampleProject projectName="Glass Break Detection" />*/}
-					{/*			<ExampleProject projectName="Glass Break Detection" />*/}
-					{/*		</div>*/}
-					{/*		<div className={`LandingPageView_gridRow2`}>*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*		</div>*/}
-					{/*		<div className={`LandingPageView_gridRow3`}>*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*		</div>*/}
-					{/*		<div className={`LandingPageView_gridRow4`}>*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*		</div>*/}
-					{/*		<div className={`LandingPageView_gridRow5`}>*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*			<ExampleProject projectName="Alexa Voice" />*/}
-					{/*		</div>*/}
-					{/*	</div>*/}
 				</div>
 			</div>
 		</>
