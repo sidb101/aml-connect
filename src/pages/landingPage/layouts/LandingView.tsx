1887bf7ca9c691ffc8809b6f9d7b406ea5cdf6a6
Troy Burgess <tburgess@andrew.cmu.edu>
2023-07-28 18:41:45 -0400

    feature[A2M-537]: Changed buttons to labels

--- src/pages/landingPage/layouts/LandingView.tsx
+++ src/pages/landingPage/layouts/LandingView.tsx
@@ -1,8 +1,54 @@
 import { LandingPageHeader } from "./components/LandingPageHeader";
-import React from "react";
 import "./LandingView.scss";
 import Header from "../../../components/header/Header";
-import ProjectCard from "./components/ProjectCard";
+import ProjectCard, { type ProjectCardT } from "./components/ProjectCard";
+
+const projectCards: ProjectCardT[][] = [
+	[
+		{
+			projectTitle: "Example Project: Dog Bark Detection",
+			projectLabel: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
+			projectDescription:
+				"An AnalogML model that detects the sound of dog bark for deployment in the outdoor context. It is " +
+				"trained on a near and far field dataset of dog barks of common American dog breeds. It employs 4 analog " +
+				"features and uses a 2-layer neural network.",
+		},
+		{
+			projectTitle: "Alexa Wakeword Detection",
+			projectLabel: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
+			projectDescription:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus mauris ultrices eros in cursus turpis massa. Vulputate enim nulla aliquet porttitor lacus luctus. Ridiculus mus mauris vitae ultricies leo. In massa tempor nec feugiat nisl pretium. Ac turpis egestas maecenas pharetra convallis.",
+		},
+	],
+	[
+		{
+			projectTitle: "Glass Break Detection",
+			projectLabel: ["1st Label", "2nd Label", "3rd Label"],
+			projectDescription:
+				"Ultricies mi eget mauris pharetra. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper feugiat. Etiam tempor orci eu lobortis elementum. Lorem mollis aliquam ut porttitor leo. Euismod elementum nisi quis eleifend.",
+		},
+		{
+			projectTitle: "Lorem ipsum dolor",
+			projectLabel: ["1st Label", "2nd Label", "3rd Label"],
+			projectDescription:
+				"Ultricies mi eget mauris pharetra. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper feugiat. Etiam tempor orci eu lobortis elementum. Lorem mollis aliquam ut porttitor leo. Euismod elementum nisi quis eleifend.",
+		},
+	],
+	[
+		{
+			projectTitle: "Vulputate enim nulla",
+			projectLabel: ["1st Label", "2nd Label", "3rd Label"],
+			projectDescription:
+				"Ultricies mi eget mauris pharetra. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper feugiat. Etiam tempor orci eu lobortis elementum. Lorem mollis aliquam ut porttitor leo. Euismod elementum nisi quis eleifend.",
+		},
+		{
+			projectTitle: "Ligula ullamcorper",
+			projectLabel: ["1st Label", "2nd Label", "3rd Label"],
+			projectDescription:
+				"Ultricies mi eget mauris pharetra. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper feugiat. Etiam tempor orci eu lobortis elementum. Lorem mollis aliquam ut porttitor leo. Euismod elementum nisi quis eleifend.",
+		},
+	],
+];
 
 export type LandingPageViewT = {
 	data?: string;
@@ -15,18 +61,12 @@ const LandingView = (props: LandingPageViewT) => {
 			<div className={`body-content-container-no-footer`}>
 				<div className={`main-content-container`}>
 					<div className={`LandingView_container`}>
-						<div className={`LandingView_containerRow`}>
-							<ProjectCard />
-							<ProjectCard />
-						</div>
-						<div className={`LandingView_containerRow`}>
-							<ProjectCard />
-							<ProjectCard />
-						</div>
-						<div className={`LandingView_containerRow`}>
-							<ProjectCard />
-							<ProjectCard />
-						</div>
+						{projectCards.map((projectCards: ProjectCardT[], index: number) => (
+							<div key={index} className={`LandingView_containerRow`}>
+								<ProjectCard projectCard={projectCards[0]} />
+								<ProjectCard projectCard={projectCards[1]} />
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
--- src/pages/landingPage/layouts/components/ProjectCard.tsx
+++ src/pages/landingPage/layouts/components/ProjectCard.tsx
@@ -1,19 +1,27 @@
 import "./ProjectCard.scss";
 
-export default function ProjectCard() {
+export type ProjectCardT = {
+	projectTitle: string;
+	projectLabel?: string[];
+	projectDescription: string;
+};
+
+type ProjectCardProps = {
+	projectCard: ProjectCardT;
+};
+
+export default function ProjectCard({ projectCard }: ProjectCardProps) {
 	return (
 		<div className={`white-panel ProjectCard_container`}>
-			<div className={`section-heading-text`}>Example Project: Dog Bark Detection</div>
+			<div className={`section-heading-text`}>{projectCard.projectTitle}</div>
 			<div className={`ProjectCard_labels`}>
-				<label className={`btn btn-solid`}>94.3% Accuracy</label>
-				<label className={`btn btn-solid`}>97.6% True Positive Rate</label>
-				<label className={`btn btn-solid`}>3sec in 24hr False Positive Rate</label>
+				{projectCard.projectLabel?.map((label: string, index: number) => (
+					<label key={index} className={`ProjectCard_label`}>
+						{label}
+					</label>
+				))}
 			</div>
-			<div className={`regular-text grey-text`}>
-				An AnalogML model that detects the sound of dog bark for deployment in the outdoor context. It is
-				trained on a near and far field dataset of dog barks of common American dog breeds. It employs 4 analog
-				features and uses a 2-layer neural network.
-			</div>
+			<div className={`regular-text grey-text`}>{projectCard.projectDescription}</div>
 			<div className={`ProjectCard_button`}>
 				<button className={`btn btn-outline`}>Open Project</button>
 			</div>
