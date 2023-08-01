298bcde7b602ab8def77ef20ac2ffbde692c959a
Troy Burgess <tburgess@andrew.cmu.edu>
2023-07-31 21:31:48 -0400

    feature[A2M-537]: Moved mock data to mockdata dir

--- src/pages/landingPage/layouts/LandingView.tsx
+++ src/pages/landingPage/layouts/LandingView.tsx
@@ -3,28 +3,18 @@ import "./LandingView.scss";
 import Header from "../../../components/header/Header";
 import ProjectCard, { type ProjectCardT } from "./components/ProjectCard";
 
-const projectCards: ProjectCardT[] = [
-	{
-		projectSlug: "project-1",
-		projectTitle: "Glass Break Detection",
-		projectLabel: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
-		projectDescription:
-			"We are developing a smart device for in-home surveillance. This device would detect the sounds of glass breaking to help trigger a security alert within our system.",
-	},
-];
-
-export type LandingPageViewT = {
-	data?: string;
+type LandingPageViewProps = {
+	projectCards?: ProjectCardT[];
 };
 
-const LandingView = (props: LandingPageViewT) => {
+const LandingView = ({ projectCards }: LandingPageViewProps) => {
 	return (
 		<>
 			<Header headerTitle={"Projects"} element={<LandingPageHeader />} />
 			<div className={`body-content-container-no-footer`}>
 				<div className={`main-content-container`}>
 					<div className={`LandingView_container`}>
-						{projectCards.map((projectCard: ProjectCardT, index: number) => (
+						{projectCards?.map((projectCard: ProjectCardT, index: number) => (
 							<ProjectCard key={index} projectCard={projectCard} />
 						))}
 					</div>
---
+++ src/tests/mockdata/allProjectCards.ts
@@ -0,0 +1,11 @@
+import { type ProjectCardT } from "../../pages/landingPage/layouts/components/ProjectCard";
+
+export const projectCards: ProjectCardT[] = [
+	{
+		projectSlug: "project-1",
+		projectTitle: "Glass Break Detection",
+		projectLabel: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
+		projectDescription:
+			"We are developing a smart device for in-home surveillance. This device would detect the sounds of glass breaking to help trigger a security alert within our system.",
+	},
+];
