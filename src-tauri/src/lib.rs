2bc7c8b1ade7a0504e434bae4294e294190e0a6d
sidb101 <bhatnagar011096@gmail.com>
2023-08-01 09:54:15 -0400

    feature/upload-files[A2M-351]: Added validation logic

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,51 +1,13 @@
 pub mod aml_core;
 
 pub mod uicontroller {
-    use crate::aml_core::element_repository::*;
-    use crate::aml_core::data_manager::*;
-    use serde::{Deserialize, Serialize};
-    use ts_rs::TS;
 
-    
-    #[derive(Debug, Serialize, Deserialize, TS)]
-    #[ts(export)]
-    enum DataSet{
-        Testing,
-        Validation,
-        Training,
-    }
-
-    
-    #[derive(Debug, Serialize, Deserialize, TS)]
-    #[ts(export)]
-    pub enum Error {
-        FileNotFound,
-        ProcessingError,
-        // Add more error variants as needed.
-    }
-
-    
-    #[derive(Debug, Serialize, Deserialize, TS)]
-    #[ts(export)]
-    pub struct File{
-        file_abs_url: String,         //absolute file paths of input files (e.g. audio files)
-        dataset_type: DataSet,
-        is_uploaded: bool,
-        file_rel_url: String,
-    }
-
-    
-    #[derive(Debug, Serialize, Deserialize, TS)]
-    #[ts(export)]
-    pub struct InputJSON{
-        proj_slug: String,
-        input_files: Vec<File>,
-        base_dir: String,       //absolute file path to the base directory for storing server side files
-    }
+    use crate::aml_core::data_manager;
+    use crate::aml_core::element_repository;
 
     #[tauri::command]
     pub fn get_elements() -> String {
-        if let Some(body) = list_elements_from_simulator() {
+        if let Some(body) = element_repository::list_elements_from_simulator() {
             format!("{{\"response\":{value}}}", value = body)
         } else {
             format!(
@@ -56,36 +18,9 @@ pub mod uicontroller {
     }
 
     #[tauri::command]
-    pub fn upload_input_files(mut input: InputJSON) -> Result<InputJSON, Error> {
-    
-
-        //TODO: validation of the list of input absolute file paths
-        
-        //TODO: copy to server side folder
-        for file in &mut input.input_files {
-            store_in_app_data(&file.file_abs_url, &input.base_dir);
-        }
-        //TODO: save metadata to db
-        
-        //TODO: return list of relative paths to server side files as OutputJSON
-        
-        //TEMP
-        // Return the OutputJSON struct
-        // for file in &mut input.input_files {
-        //     file.file_rel_url = format!("uploads/{}", file.file_abs_url);
-        //     file.is_uploaded = true;
-        // }
-    
-    
-        dbg!(&input);
-    
-        let success = true;
-        if(success) {
-            Ok(input)
-        } else {
-            Err(Error::ProcessingError)
-        }
-    
-        
+    pub fn put_files(
+        input: data_manager::FilesUploadRequest,
+    ) -> data_manager::SaveFilesResponse {
+        data_manager::save_input_files(&input)
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use aml_connect::uicontroller;
+use directories::ProjectDirs;
 
 fn main() {
+    // let proj_dirs = ProjectDirs::from("com", "aspinity", "aml_connect").unwrap();
+    // println!("{}", format!("{}/",proj_dirs.data_local_dir().display()).as_str());
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![uicontroller::get_elements])
         .run(tauri::generate_context!())
