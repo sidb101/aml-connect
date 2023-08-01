25fe66d71d938fecdd012202037c925730f38731
Troy Burgess <tburgess@andrew.cmu.edu>
2023-08-01 12:18:34 -0400

    feature[A2M-537]: Extracted common display card component

--- src/tests/mockdata/allProjectCards.ts
+++ src/tests/mockdata/allProjectCards.ts
@@ -1,11 +1,13 @@
-import { type ProjectCardT } from "../../pages/landingPage/layouts/components/ProjectCard";
+import { type DisplayCardT } from "../../components/displayCard/DisplayCard";
+import { projectOverviewRoute } from "../../routes";
 
-export const projectCards: ProjectCardT[] = [
+export const projectCards: DisplayCardT[] = [
 	{
-		projectSlug: "project-1",
-		projectTitle: "Glass Break Detection",
-		projectLabel: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
-		projectDescription:
+		route: projectOverviewRoute("project-1"),
+		title: "Glass Break Detection",
+		labels: ["94.3% Accuracy", "97.6% True Positive Rate", "3sec in 24hr False Positive Rate"],
+		description:
 			"We are developing a smart device for in-home surveillance. This device would detect the sounds of glass breaking to help trigger a security alert within our system.",
+		buttonText: "Open Project",
 	},
 ];
