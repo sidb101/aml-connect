21669a54ccf518851c37f15e7643c7217babc63d
Troy Burgess <tburgess@andrew.cmu.edu>
2023-08-02 19:59:02 -0400

    feature[A2M-676]: Accordions can now use a percentage or a fixed

--- src/pages/dataHubPage/layouts/DataSetup/DataSetupView.tsx
+++ src/pages/dataHubPage/layouts/DataSetup/DataSetupView.tsx
@@ -2,6 +2,7 @@ import "./DataSetupView.scss";
 import React from "react";
 import Accordion from "../../../../components/accordion/Accordion";
 import FileTable, { type AudioFileT } from "./FileTable";
+import NewAccordion from "../../../../components/accordion/NewAccordion";
 
 export type DataSetupViewProps = {
 	audioFiles?: AudioFileT[];
@@ -11,22 +12,19 @@ const DataSetupView = ({ audioFiles }: DataSetupViewProps) => {
 	return (
 		<div className={`DataSetupView_container`}>
 			<div className={`DataSetupView_leftContainer`}>
-				<Accordion
-					headerElement={<>Training Dataset</>}
-					maxHeight={"200px"}
-					bodyElement={audioFiles && <FileTable files={audioFiles} />}
-				/>
-				<Accordion
-					headerElement={<>Validation Dataset</>}
-					bodyElement={audioFiles && <FileTable files={audioFiles} />}
-					maxHeight={"300px"}
+				<NewAccordion bodyMaxHeight={`calc(33vh - 150px)`} header={<>Training Dataset</>}>
+					<FileTable files={audioFiles} />
+				</NewAccordion>
+				<NewAccordion
+					bodyMaxHeight={`calc(33vh - 150px)`}
 					defaultIsOpen={false}
-				/>
-				<Accordion
-					headerElement={<>Testing Dataset</>}
-					bodyElement={audioFiles && <FileTable files={audioFiles} />}
-					defaultIsOpen={false}
-				/>
+					header={<>Validation Dataset</>}
+				>
+					<FileTable files={audioFiles} />
+				</NewAccordion>
+				<NewAccordion bodyMaxHeight={`calc(33vh - 150px)`} defaultIsOpen={false} header={<>Testing Dataset</>}>
+					<FileTable files={audioFiles} />
+				</NewAccordion>
 			</div>
 			<div className={`DataSetupView_rightContainer`}>
 				<Accordion headerElement={<>Add or Merge Data</>} bodyElement={<h4>Data Body</h4>} />
