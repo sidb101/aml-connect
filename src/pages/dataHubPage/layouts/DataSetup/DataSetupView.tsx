0b7fe926bbbdaf9c2ddd24e279f533133c98b1c7
DisguisedFlyer <tburgess@gmail.com>
2023-08-03 14:20:18 -0400

    feature[A2M-676]: Removing the old accordion

--- src/pages/dataHubPage/layouts/DataSetup/DataSetupView.tsx
+++ src/pages/dataHubPage/layouts/DataSetup/DataSetupView.tsx
@@ -1,8 +1,7 @@
 import "./DataSetupView.scss";
 import React from "react";
-import Accordion from "../../../../components/accordion/Accordion";
 import AudioFileTable from "./AudioFileTable";
-import NewAccordion from "../../../../components/accordion/NewAccordion";
+import Accordion from "../../../../components/accordion/Accordion";
 import type { InputFileDataT } from "../../../../redux/slices/DataHubSlice";
 
 export type DataSetupViewProps = {
@@ -13,23 +12,23 @@ const DataSetupView = ({ audioFiles }: DataSetupViewProps) => {
 	return (
 		<div className={`DataSetupView_container`}>
 			<div className={`DataSetupView_leftContainer`}>
-				<NewAccordion bodyMaxHeight={`calc(33vh - 150px)`} header={<>Training Dataset</>}>
+				<Accordion bodyMaxHeight={`calc(33vh - 150px)`} header={<>Training Dataset</>}>
 					<AudioFileTable files={audioFiles} />
-				</NewAccordion>
-				<NewAccordion
-					bodyMaxHeight={`calc(33vh - 150px)`}
-					defaultIsOpen={false}
-					header={<>Validation Dataset</>}
-				>
+				</Accordion>
+				<Accordion bodyMaxHeight={`calc(33vh - 150px)`} defaultIsOpen={false} header={<>Validation Dataset</>}>
 					<AudioFileTable files={audioFiles} />
-				</NewAccordion>
-				<NewAccordion bodyMaxHeight={`calc(33vh - 150px)`} defaultIsOpen={false} header={<>Testing Dataset</>}>
+				</Accordion>
+				<Accordion bodyMaxHeight={`calc(33vh - 150px)`} defaultIsOpen={false} header={<>Testing Dataset</>}>
 					<AudioFileTable files={audioFiles} />
-				</NewAccordion>
+				</Accordion>
 			</div>
 			<div className={`DataSetupView_rightContainer`}>
-				<Accordion headerElement={<>Add or Merge Data</>} bodyElement={<h4>Data Body</h4>} />
-				<Accordion headerElement={<>Label Data</>} bodyElement={<h4>Label Body</h4>} defaultIsOpen={false} />
+				<Accordion header={<>Add or Merge Data</>}>
+					<h4>Data Body</h4>
+				</Accordion>
+				<Accordion header={<>Label Data</>} defaultIsOpen={false}>
+					<h4>Label Body</h4>
+				</Accordion>
 			</div>
 		</div>
 	);
