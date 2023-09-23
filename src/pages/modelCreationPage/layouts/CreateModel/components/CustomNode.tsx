23636331a0ec92884ecd74679e2c10329d41e101
Troy G. Burgess <tburgess@andrew.cmu.edu>
2023-09-22 22:50:02 -0400

    feature[A2M-709]: Fixed issue in the CustomNode component

--- src/pages/modelCreationPage/layouts/CreateModel/components/CustomNode.tsx
+++ src/pages/modelCreationPage/layouts/CreateModel/components/CustomNode.tsx
@@ -1,14 +1,14 @@
 import type { NodeProps } from "reactflow";
-import { Handle } from "reactflow";
+import { Handle, Position } from "reactflow";
 import "./CustomNode.scss";
 export default function CustomNode({ data }: NodeProps) {
 	return (
 		<div className={`CustomNode`}>
 			{/*<Handle type="target" position="left" style={{ borderRadius: 0 }} />*/}
-			<Handle type="target" position="left" />
+			<Handle type="target" position={Position.Left} />
 			<div>{data.label}</div>
 			{/*<Handle type="source" position="right" style={{ borderRadius: 0 }} />*/}
-			<Handle type="source" position="right" />
+			<Handle type="source" position={Position.Right} />
 		</div>
 	);
 }
