ff5a5eeac7a89ba1e4e7041e811a05737dd72714
sidb101 <bhatnagar011096@gmail.com>
2023-10-23 00:00:08 -0400

    Moved tests files to dedicated folder

--- aml_simulator_pywrapper/test/test_simulate_network.py
+++ aml_simulator_pywrapper/test/test_simulate_network.py
@@ -4,7 +4,7 @@ import os
 
 def test_simulate_network():
     # arrange
-    network_json_path = os.path.join(os.getcwd(), "test_network.json")
+    network_json_path = os.path.join(os.getcwd(), "test/test_network.json")
     parent_dir = os.path.dirname(os.getcwd())
     audio_file_path = os.path.join(parent_dir, "src-tauri", "tests", "rising-chirp.wav")
     expected_keys = set(['vdd', 'filter_out_hpf', 'mid', '', '_lpf_pre_compress', 'in', 'Gnd', 'filter_out_pre_compress', 'filter_out_bpf', 'out', 'filter_out'])
