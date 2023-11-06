998b3342b89c68fd7f9579125353923dac350a60
Siddharth Bhatnagar <bhatnagar011096@gmail.com>
2023-11-06 16:54:21 -0500

    get_elements v2: Now serves static, type-checked JSON instead of sidecar (#41)
    * feature[A2M-809]: starter get_elements json
    
    * chore[A2M-786] Added the First two components
    
    * feature[A2M-809]: added 5 more element metadata info in json
    
    * chore[A2M-786] Added some more components
    
    * feature[A2M-809]: Rust type-check + serve json
    
    * chore[A2M-786] Corrected Delayed Flip-flop
    
    * feature[A2M-809]: Included revised elements.json, schema changes
    
    * feature[A2M-809]: Removed opamp_implementation, Added Terminal
    
    * Fixed typo
    
    * chore[A2M-787] Fixed direction optional (By Jyot)
    
    * Made params optional in elementMetadata
    
    * chore[A2M-357] Fixed Get Elements Mock Data
    
    * Removed unwanted exports
    
    * Deleted unwanted ts exports
    
    ---------
    
    Co-authored-by: JAshMe <mailme.jyotmehta@gmail.com>
    Co-authored-by: Henk Hsiung <haihsiah@uci.edu>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,15 @@
 pub mod aml_core;
 
 pub mod uicontroller {
-    use serde_json;
-
     use crate::aml_core::element_repository;
-    use crate::aml_core::network_manager::SimulatorError;
     use crate::aml_core::file_data_manager;
     use tauri::State;
     use diesel::r2d2::{ConnectionManager, Pool};
     use diesel::SqliteConnection;
     
     #[tauri::command]
-    pub fn get_elements() -> Result<serde_json::Value, SimulatorError> {
-        let elements_json = element_repository::list_elements_from_simulator()?;
-        Ok(elements_json)
+    pub fn get_elements() -> element_repository::GetElementsResponseResult {
+        element_repository::list_elements_from_simulator()
     }
 
     #[tauri::command]
