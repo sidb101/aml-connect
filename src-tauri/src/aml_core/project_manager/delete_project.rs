fb66a34b399f606b4cb80e21ab284d9594feb991
Gaurav Agerwala <gauravagerwala@gmail.com>
2023-12-07 22:42:34 -0500

    fixed delete proejct

--- src-tauri/src/aml_core/project_manager/delete_project.rs
+++ src-tauri/src/aml_core/project_manager/delete_project.rs
@@ -1,7 +1,7 @@
-use diesel::{QueryDsl, RunQueryDsl};
+use diesel::{QueryDsl, RunQueryDsl, ExpressionMethods};
 
 use crate::aml_core::{
-    db_adapter::{models::Project, schema::projects, DbConn},
+    db_adapter::{models::Project, schema::{projects, audio_files, input_data}, DbConn},
     file_data_manager,
     project_manager::{DeleteProjectResponse, ProjectManagerError},
     AppError,
@@ -32,6 +32,47 @@ pub fn delete_project(
             ))
         }
     };
+
+    // Deleting audio files and input data mapped to this project from DB. disk will be deleted later.
+    // Get input data records mapped to this project
+    let input_data_records = match input_data::table
+        .filter(input_data::project_id.eq(project.id))
+        .load::<crate::aml_core::db_adapter::models::InputData>(conn)
+    {
+        Ok(input_data_records) => input_data_records,
+        Err(error) => {
+            log::error!("error getting input data records: {}", error);
+            return Err(AppError::ProjectManagerError(
+                ProjectManagerError::InternalError(error.to_string()),
+            ));
+        }
+    };
+
+    // Delete audio files mapped to each input data record
+    for input_data_record in input_data_records {
+        match diesel::delete(audio_files::table.filter(audio_files::input_data_id.eq(input_data_record.id)))
+            .execute(conn)
+        {
+            Ok(_) => log::info!("audio files deleted"),
+            Err(error) => {
+                log::error!("error deleting audio files: {}", error);
+                return Err(AppError::ProjectManagerError(
+                    ProjectManagerError::InternalError(error.to_string()),
+                ));
+            }
+        }
+    }
+
+    // Delete input data records
+    match diesel::delete(input_data::table.filter(input_data::project_id.eq(project.id))).execute(conn) {
+        Ok(_) => log::info!("input data records deleted"),
+        Err(error) => {
+            log::error!("error deleting input data records: {}", error);
+            return Err(AppError::ProjectManagerError(
+                ProjectManagerError::InternalError(error.to_string()),
+            ));
+        }
+    }
 
     match diesel::delete(&project).execute(conn) {
         Ok(_) => {
